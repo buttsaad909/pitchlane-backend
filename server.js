b4d41bd0eaea8a697d5135db585c4eaec91d47f5
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-09-04 17:34:44 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server Listening on port ${PORT}`);
+  });
